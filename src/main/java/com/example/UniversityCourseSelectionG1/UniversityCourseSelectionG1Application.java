243db43e6fca5bbde66bb4a22453282a6093094e
prashant101020 <pyss0206@gmail.com>
2022-10-31 11:12:04 +0530

    first commit

---
+++ src/main/java/com/example/UniversityCourseSelectionG1/UniversityCourseSelectionG1Application.java
@@ -0,0 +1,13 @@
+package com.example.UniversityCourseSelectionG1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityCourseSelectionG1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityCourseSelectionG1Application.class, args);
+	}
+
+}
